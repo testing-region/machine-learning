de09f81a8cfbf520799aaf672cb2f00024421314
David Saah <dasorange.hope@gmail.com>
2022-02-02 19:01:59 +0000

    remove unnecessary comments

--- Linear Regression/Gradient Descent Single Variable/computeCost.m
+++ Linear Regression/Gradient Descent Single Variable/computeCost.m
@@ -3,7 +3,6 @@ function J = computeCost(X, y, theta)
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
-% Initialize some useful values
 m = length(y); % number of training examples
 J = 0;
 
